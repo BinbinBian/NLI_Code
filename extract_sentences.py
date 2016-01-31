dd9689dfd637fc3a935a8e0f33aa13ae95a69c39
David <torrejonmoya.david@gmail.com>
2016-01-31 19:08:46 +0100

    left file with columns needed

--- extract_sentences.py
+++ extract_sentences.py
@@ -17,6 +17,7 @@ file_to_read = 'snli_1.0/snli_1.0_train.jsonl'
 
 try:
     #read whole file into python array
+    print 'Opening File ' + file_to_read
     with open(file_to_read, 'rb') as f:
         data = f.readlines()
     '''
@@ -32,6 +33,20 @@ try:
     data_df = pd.read_json(data_json_str)
 
     # Only need sentence 1 and sentence 2 + id + annotator_labels
-    print data_df[0]
+    """
+    {"annotator_labels": ["neutral"], "captionID": "3416050480.jpg#4", "gold_label": "neutral", "pairID": "3416050480.jpg#4r1n", "sentence1": "A person on a horse jumps over a broken down airplane.",
+    "sentence1_binary_parse": "( ( ( A person ) ( on ( a horse ) ) ) ( ( jumps ( over ( a ( broken ( down airplane ) ) ) ) ) . ) )",
+    "sentence1_parse": "(ROOT (S (NP (NP (DT A) (NN person)) (PP (IN on) (NP (DT a) (NN horse)))) (VP (VBZ jumps) (PP (IN over) (NP (DT a) (JJ broken) (JJ down) (NN airplane)))) (. .)))",
+    "sentence2": "A person is training his horse for a competition.", "sentence2_binary_parse": "( ( A person ) ( ( is ( ( training ( his horse ) ) ( for ( a competition ) ) ) ) . ) )",
+    "sentence2_parse": "(ROOT (S (NP (DT A) (NN person)) (VP (VBZ is) (VP (VBG training) (NP (PRP$ his) (NN horse)) (PP (IN for) (NP (DT a) (NN competition))))) (. .)))"}
+    """
+    # ,['sentence1_binary_parse'],['sentence1_parse'],['sentence2_binary_parse'],['sentence2_parse']
+    del data_df['captionID']
+    del data_df['sentence1_binary_parse']
+    del data_df['sentence1_parse']
+    del data_df['sentence2_binary_parse']
+    del data_df['sentence2_parse']
+    print data_df.head(3)
+    print data_df.tail(3)
 except IOError as e:
     print e
